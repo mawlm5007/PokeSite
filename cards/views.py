ff8ac2a30db6663a2a363cf061a6c7dc995c36b3
mawlm5007 <maahirmawla24@gmail.com>
2022-08-14 17:34:50 -0400

    added modesl and index page, pre authenitcation

--- cards/views.py
+++ cards/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'cards/index.html')
+
+def reports(request):
+    return render(request, 'cards/reports.html')
